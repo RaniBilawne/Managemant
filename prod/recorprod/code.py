5df976e354b29c7c1e7d90482a3f463548145df3
ranibilawne <prachibilawne@gmail.com>
2018-02-15 19:40:30 +0530

    New code added in new branch

---
+++ prod/recorprod/code.py
@@ -0,0 +1,4 @@
+#/usr/bin/python
+
+print "hello i am in recordprod"
+print "i am testing for branch"
---
+++ prod2/methodtest.py
@@ -0,0 +1,14 @@
+#/usr/bin/python
+
+class TestClass:
+   def init(self):
+      print "I am in class TestClass"
+   def newmethod(self):
+       print "I am in new method"
+
+object1 = TestClass()
+object1.newmethod()
+
+
+
+
